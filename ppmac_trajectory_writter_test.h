6c0eb7e1a762dd8e3f9afd9014eef10791e937f2
Leandro Martins dos Santos <Leandro.Martins-dos-Santos@diamond.ac.uk>
2025-05-02 17:05:40 +0000

    Add header guard

--- ppmac_trajectory_writter_test.h
+++ ppmac_trajectory_writter_test.h
@@ -1,18 +1,20 @@
+#ifndef TRAJ_WRITTER_H
+#define TRAJ_WRITTER_H
+
 #define TEST_ENV
 
 #ifdef TEST_ENV
-    # define MAX_P 65536
-    struct SHM {
-        double P[MAX_P];            // Global P variable Array
-    };
-    struct SHM  *pshm;              // Pointer to shared memory
-    void        *pushm;             // Pointer to user shared memory
-
     #include "Libraries/ppmac_tcp/ppmac_tcp.h"
     #include "Libraries/ppmac_ushm_buffer/ppmac_ushm_buffer.h"
+
 #else
     #define RUN_AS_RT_APP
 
     #include "../../Libraries/ppmac_tcp/ppmac_tcp.h"
     #include "../../Libraries/ppmac_ushm_buffer/ppmac_ushm_buffer.h"
-#endif // TEST_ENV
+
+    #endif // TEST_ENV
+    int serverSock;
+
+#endif // TRAJ_WRITTER_H
+
